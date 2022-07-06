f7732d02773975342cf0ab8a5f431ebc52b3feb4
ShiraOhana <soshiraohana@gmail.com>
2022-07-06 16:15:48 +0300

    react setup

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import Landing from "./pages/landing";
+
+function App() {
+  return (
+    <div>
+      {" "}
+      <h1>Jobily</h1>
+      <Landing />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/pages/landing.js
@@ -0,0 +1,33 @@
+import React from "react";
+import logo from "../assets/images/logo.svg";
+import main from "../assets/images/main.svg";
+
+const Landing = () => {
+  return (
+    <main>
+      <nav>
+        <img src={logo} alt="jobily" className="logo"></img>
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            Job <span>Tracking</span> App
+          </h1>
+          <p>
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum."
+          </p>
+          <button className="btn btn-hero">Login / Register</button>
+          <img src={main} alt="Job tracker" className="img main-img"></img>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Landing;
