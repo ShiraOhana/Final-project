ed921c7c5fb768bd393e5096a573a95705e54eb4
ShiraOhana <soshiraohana@gmail.com>
2022-07-08 00:19:20 +0300

    register form

--- client/src/pages/register.js
+++ client/src/pages/register.js
@@ -11,7 +11,7 @@ const initialState = {
 };
 
 const Register = () => {
-  const [value, setValue] = useState(initialState);
+  const [values, setValues] = useState(initialState);
   // global state and useNavigate
 
   const handleChange = (e) => {
@@ -22,7 +22,29 @@ const Register = () => {
     console.log(e.target);
   };
 
-  return <Wrapper className="full-page">Register</Wrapper>;
+  return (
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={onSubmit}>
+        <Logo />
+        <h3>Login</h3>
+        <div className="form-row">
+          <label htmlFor="name" className="form-lable">
+            Name
+          </label>
+          <input
+            type="text"
+            value={values.name}
+            name="name"
+            onChange={handleChange}
+            className="form-input"
+          />
+        </div>
+        <button type="submit" className="btn btn-block">
+          Submit
+        </button>
+      </form>
+    </Wrapper>
+  );
 };
 
 export default Register;
