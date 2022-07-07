15b94bcf922971cf0037cb897bf64d2653c5bb8c
ShiraOhana <soshiraohana@gmail.com>
2022-07-08 00:11:29 +0300

    register page logic

--- client/src/pages/register.js
+++ client/src/pages/register.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { Logo } from "../components";
+import Wrapper from "../assets/wrappers/RegisterPage";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+};
 
 const Register = () => {
-  return <h1>Register</h1>;
+  const [value, setValue] = useState(initialState);
+  // global state and useNavigate
+
+  const handleChange = (e) => {
+    console.log(e.target);
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+
+  return <Wrapper className="full-page">Register</Wrapper>;
 };
 
 export default Register;
