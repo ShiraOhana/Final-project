0ea081c0d416260820576897d209d30768e83986
ShiraOhana <soshiraohana@gmail.com>
2022-07-07 22:18:16 +0300

    routes

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,15 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 function App() {
   return (
     <div>
-      <Landing />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<div>Dashboard</div>} />
+          <Route path="/register" element={<div>Register</div>} />
+          <Route path="/landing" element={<Landing />} />
+
+          {/* <Landing /> */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
