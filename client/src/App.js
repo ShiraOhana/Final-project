a5333b2438f8bfbe86f42c835820e507942079e1
ShiraOhana <soshiraohana@gmail.com>
2022-07-07 23:46:37 +0300

    error page

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,20 @@
-import Landing from "./pages/landing";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Dashboard, Register, Landing, Error } from "./pages";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <nav>
+        {/* <nav>
           <Link to="/">Dashboard</Link>
           <Link to="/register">Register</Link>
           <Link to="/landing">Landing</Link>
-        </nav>
+        </nav> */}
         <Routes>
-          <Route path="/" element={<div>Dashboard</div>} />
-          <Route path="/register" element={<div>Register</div>} />
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/register" element={<Register />} />
           <Route path="/landing" element={<Landing />} />
-          <Route path="*" element={<div>Error</div>} />
-
-          {/* <Landing /> */}
+          <Route path="*" element={<Error />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ client/src/pages/index.js
@@ -0,0 +1,6 @@
+import Dashboard from "./dashboard";
+import Landing from "./landing";
+import Error from "./Error";
+import Register from "./register";
+
+export { Dashboard, Landing, Error, Register };
