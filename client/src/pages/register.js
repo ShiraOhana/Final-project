f9132c585f6db390fe770efae381cdb947321f44
ShiraOhana <soshiraohana@gmail.com>
2022-07-10 20:18:35 +0300

    global context

--- client/src/pages/register.js
+++ client/src/pages/register.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { Logo, FormRow, Alert } from "../components";
 import Wrapper from "../assets/wrappers/RegisterPage";
+import { useAppContext } from "../context/appContext";
 
 const initialState = {
   name: "",
@@ -14,6 +15,7 @@ const initialState = {
 const Register = () => {
   const [values, setValues] = useState(initialState);
   // global state and useNavigate
+  const state = useAppContext();
 
   const toggleMember = () => {
     setValues({ ...values, isMember: !values.isMember });
