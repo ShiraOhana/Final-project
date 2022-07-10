1f9c4f166dd41bc48cf64e914c479c8abaf7a762
ShiraOhana <soshiraohana@gmail.com>
2022-07-10 20:54:35 +0300

    alert for register

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -1,5 +1,8 @@
 import React, { useContext, useReducer } from "react";
+import { DISPLAY_ALERT } from "./actions";
+
 import reducer from "./reducer";
+
 const initialState = {
   isLoading: false,
   showAlert: false,
@@ -12,8 +15,14 @@ const AppContext = React.createContext();
 const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  const displayAlert = () => {
+    dispatch({ type: DISPLAY_ALERT });
+  };
+
   return (
-    <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>
+    <AppContext.Provider value={{ ...state, displayAlert }}>
+      {children}
+    </AppContext.Provider>
   );
 };
 
