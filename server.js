5de9cd0bd07f4f858d66e8df42e3ef5821a0c3a1
ShiraOhana <soshiraohana@gmail.com>
2022-07-11 15:33:40 +0300

    env

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 import express from "express";
 const app = express();
+import dotenv from "dotenv";
+dotenv.config();
 
 //middleware
 import notFoundMiddleware from "./middleware/not-found.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
 
 app.get("/", (req, res) => {
-  throw new Error("Error");
   res.send("Welcome!");
 });
 
