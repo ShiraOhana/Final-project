f7e4465437a28997d2bc0fed200ab43b95405544
ShiraOhana <soshiraohana@gmail.com>
2022-07-14 16:13:21 +0300

    User Schema

---
+++ models/User.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide name"],
+    minlength: 3,
+    maxlength: 20,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide a email"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+    minlength: 6,
+  },
+  lastName: {
+    type: String,
+    maxlength: 20,
+    trim: true,
+    default: "lastName",
+  },
+  location: {
+    type: String,
+    maxlength: 20,
+    trim: true,
+    default: "My city",
+  },
+});
+
+export default mongoose.model("User", UserSchema);
