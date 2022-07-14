5924137b78f556d4d845ff84676c72ac04e4b0ad
ShiraOhana <soshiraohana@gmail.com>
2022-07-14 17:56:32 +0300

    register controllers

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,13 @@
+import User from "../models/User.js";
+
 const register = (req, res) => {
-  res.send("register user");
+  try {
+const user = await User.create(req.body)
+res.status(201).json({user})
+  } catch (error) {
+    res.status(500).json({msg:"There was an error"})
+
+  }
 };
 const login = (req, res) => {
   res.send("login user");
