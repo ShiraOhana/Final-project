514395fe19ab17f8a6bd1397f0230b9154e1bedf
ShiraOhana <soshiraohana@gmail.com>
2022-07-14 14:18:10 +0300

    job router

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,1 +1,18 @@
-const creatJob = "";
+const createJob = async (req, res) => {
+  res.send("Create Job");
+};
+const getAllJobs = async (req, res) => {
+  res.send("Get all Job");
+};
+const updateJob = async (req, res) => {
+  res.send("Update Job");
+};
+const deleteJob = async (req, res) => {
+  res.send("Delete Job");
+};
+
+const showStats = async (req, res) => {
+  res.send("Show stats ");
+};
+
+export { createJob, getAllJobs, deleteJob, updateJob, showStats };
